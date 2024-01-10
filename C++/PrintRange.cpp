4b2b07fbf271c192278c91b4f30b1f0ba876f6a3
Egor <plyxueja@gmail.com>
2024-01-10 19:06:39 +0900

    Initial commit

---
+++ C++/PrintRange.cpp
@@ -0,0 +1,8 @@
+//Prints the elements in range of iterators
+template <typename It>
+void PrintRange(It range_begin, It range_end){
+    for(auto it = range_begin;it!=range_end;it++){
+        cout<<*it<<" ";
+    }
+    cout<<endl;  
+}
